2147993c3e09fb34b17c1aa87db5205683852939
Jack Wu <jackwu350@yahoo.com>
2018-06-12 17:56:00 -0700

    define user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,54 @@
 class User < ApplicationRecord
+  attr_reader :password
+
+
+  # Model level validations
+  validates :email, :session_token, :password_digest, presence: true,
+  validates :password, {minimum: 6, allow_nil: true }
+  validates :email, :session_token, uniqueness: true
+
+
+
+  before_validation :ensure_session_token
+
+
+  #setter method.
+  def password=(password)
+    @password = password
+    self.password_digest = BCrypt::Password.create(password)
+  end
+
+  def self.find_by_credentials(email,password)
+    user = User.find_by_email(email)
+
+    user && user.is_password?(password)? user : nil
+
+  end
+
+
+  def self.generate_session_token
+    SecureRandom::urlsafe_base64(16)
+  end
+
+  def reset_session_token!
+    self.session_token = User.generate_session_token
+    self.save!
+    self.session_token
+  end
+
+  def is_password?(password)
+    BCrypt::Password.new(self.password_digest).is_password?(password)
+  end
+
+  def ensure_session_token
+    self.session_token ||= User.generate_session_token
+
+  end
+
+
+
+
+
+
+
 end
